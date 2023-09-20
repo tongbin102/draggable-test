b4b51f3f4e325d3df50b21e57500f94ff195ae84
tongbin102 <tongbin102@126.com>
2023-09-20 16:05:35 +0800

    1

---
+++ draggable/src/main/java/com/demo/draggable/DraggableApplication.java
@@ -0,0 +1,13 @@
+package com.demo.draggable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraggableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DraggableApplication.class, args);
+    }
+
+}
---
+++ draggable/src/test/java/com/demo/draggable/DraggableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.draggable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraggableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
